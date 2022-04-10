e5884b6801d86751f254dcdb3f29d4ee59bcd78e
浅笑Azusa <jshahezy@gmail.com>
2022-04-11 07:53:37 +0800

    Debug - 好友备注函数使用错误

--- QQ.py
+++ QQ.py
@@ -201,14 +201,16 @@ async def recvMsg():
         nickName = json_data["sender"]["nickname"]
         msg = msgFormat(json_data["message"])
         uid = json_data["sender"]["user_id"]
-        remark = getnickname(uid)
+        remark = getremark(uid)
         if remark != "":
-            nickName = remark
-        print("来自%s的私聊消息:%s"%(nickName,msg))
+            Name = remark
+        else:
+            Name = nickName
+        print("来自%s的私聊消息:%s"%(Name,msg))
         if MiPush == "True":
-            await httpx.AsyncClient().post("https://tdtt.top/send",data={'title':nickName,'content':msg,'alias':KEY})
+            await httpx.AsyncClient().post("https://tdtt.top/send",data={'title':Name,'content':msg,'alias':KEY})
         elif FCM == "True":
-            await httpx.AsyncClient().post("https://wirepusher.com/send",data={'id':KEY,'title':nickName,'message':msg,'type':'privateMsg'})
+            await httpx.AsyncClient().post("https://wirepusher.com/send",data={'id':KEY,'title':Name,'message':msg,'type':'privateMsg'})
         elif TG == "True":
             if TG_API == "":
                 TG_API = "api.telegram.org"
