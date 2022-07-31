2070b99362aaeb350293ed1d50f6924a135b83e9
qxazusa <jshahezy@gmail.com>
2022-07-31 13:18:24 +0900

    Debug - @全体成员-文本查找错误

--- QQ.py
+++ QQ.py
@@ -63,8 +63,7 @@ def msgFormat(msg):
         msg = msg.replace(cqcode, reply_format)
     if "CQ:at" in msg:
         if "all" in msg:
-            regex1 = re.compile(r'\[CQ:at,qq=all]')
-            at_cqcode = regex1.search(msg)
+            at_cqcode = re.findall('\[CQ:at,qq=all]', msg)
             for code in at_cqcode:
                 msg = msg.replace(code, ' @全体成员 ')
         else:
